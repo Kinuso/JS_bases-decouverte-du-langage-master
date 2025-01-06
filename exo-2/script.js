9e3b52ce43dec029dfd4c792fa4bf60e23a18539
Killian <killian2908@gmail.com>
2025-01-06 11:30:38 +0100

    Ex 2 - Les conditions

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,41 @@
 // you can write js here
-console.log('exo-2');
+console.log("exo-2");
+
+const isTesting = false;
+const myDate = new Date().getDay();
+const hour = new Date().getHours();
+
+console.log(hour);
+
+console.log(myDate);
+
+const weekMessage = "Pas encore";
+
+const weekEndMessage = "C'est la teuf";
+
+function isWeekend(day, hour) {
+  if (isTesting) {
+    let usersHour = prompt(
+      "Veuillez enter l'heure (sans les minutes) actuelle"
+    );
+    let usersDay = prompt("Veuillez enter le jour actuel");
+    console.log(usersHour, usersDay);
+    if (
+      usersDay == "dimanche" ||
+      usersDay == "samedi" ||
+      (usersDay == "lundi" && usersHour < 9)
+    ) {
+      console.log(weekEndMessage);
+    } else {
+      console.log(weekMessage);
+    }
+  } else {
+    if (day === 0 || day === 6 || (day === 1 && hour < 9)) {
+      console.log(weekEndMessage);
+    } else {
+      console.log(weekMessage);
+    }
+  }
+}
+
+isWeekend(myDate, hour);

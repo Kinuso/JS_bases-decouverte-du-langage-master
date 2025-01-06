729e048299326fea0622badec5639c00cacc1743
Killian <killian2908@gmail.com>
2025-01-06 16:04:32 +0100

    Ex 5 - Les boucles

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,20 @@
 // you can write js here
 
-console.log('exo-5');
+console.log("exo-5");
+
+const vowels = ["A", "E", "I", "O", "U", "Y"];
+let input = "apfuhfpzibhzagiarukfh;cbehkgjz,ja;qfDELVBS";
+const resultArray = []
+
+input.toUpperCase().split('').forEach(element => {
+    console.log(element)
+    vowels.forEach(value => {
+        if (element == value) {
+            resultArray.push(element)
+        }
+    });
+});
+
+
+finalResult = resultArray.join('')
+console.log(finalResult)
